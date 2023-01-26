55d445e995f6b648fbee837053adc7c8e1343ede
yash-04 <82692651+yash-04@users.noreply.github.com>
2023-01-26 12:16:38 +0530

    import java.util.*; class Sample{     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int n = s.nextInt();         int arr[]= new int[n];         for(int i=0;i<n;i++){             arr[i] = s.nextInt();         }         int a= s.nextInt();         int b= s.nextInt();         int sum=0;         for(int i:arr){             if(i>=a && i<=b){                 sum+=i;             }         }         System.out.print(sum);
        }
    }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Sample{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        int a= s.nextInt();
+        int b= s.nextInt();
+        int sum=0;
+        for(int i:arr){
+            if(i>=a && i<=b){
+                sum+=i;
+            }
+        }
+        System.out.print(sum);
+        
+    }
+}
