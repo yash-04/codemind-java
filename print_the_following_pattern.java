9ad63e3aa0c841612e8f0c3b88dada4d89f88367
yash-04 <82692651+yash-04@users.noreply.github.com>
2023-01-26 11:54:02 +0530

    import java.util.Scanner; class Sample {     public char ret(char i)     {         int k=(int)i;         int j=64+k;         char c=(char)j;         return c;     }     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         for(int i=n;i>=1;i--){             for(int j=1;j<=i;j++){                 Sample obj=new Sample();                 char r1=obj.ret((char)i);                 System.out.print(r1+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 class Sample
 {
+    public char ret(char i)
+    {
+        int k=(int)i;
+        int j=64+k;
+        char c=(char)j;
+        return c;
+    }
     public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==j){
-                    System.out.print(0);
-                }
-                else{
-                    System.out.print("x");
-                }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                Sample obj=new Sample();
+                char r1=obj.ret((char)i);
+                System.out.print(r1+" ");
             }
             System.out.println();
         }        
