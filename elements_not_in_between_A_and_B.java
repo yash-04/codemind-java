cbec841773c5849d80b45fbee57c349c7fa5fdad
yash-04 <82692651+yash-04@users.noreply.github.com>
2023-01-30 21:00:19 +0530

    import java.util.*; class Sample {     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int n =  s.nextInt();         int arr[] = new int[n];         for(int i=0;i<n;i++){             arr[i] = s.nextInt();         }         int a = s.nextInt();         int b = s.nextInt();         int cnt=0;         for(int i:arr){             if(i<a || i>b){               System.out.print(i + " ");               cnt++;             }         }         if(cnt==0){              System.out.print(-1);         }
        }
    }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sample {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n =  s.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int cnt=0;
+        for(int i:arr){
+            if(i<a || i>b){
+              System.out.print(i + " ");
+              cnt++;
+            } 
+        }
+        if(cnt==0){
+             System.out.print(-1);
+        }
+       
+    }
+}
